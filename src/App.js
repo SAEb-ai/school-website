06a77fa412f35f6143564c5e8122f4b6a076f302
Md Shahbaz Alam <shahbazalam75508@gmail.com>
2021-08-21 18:55:47 +0530

    Commit

--- src/App.js
+++ src/App.js
@@ -11,8 +11,12 @@ import third from "./images/third.jpeg";
 import fourth from "./images/fourth.jpeg";
 import { Card } from "react-bootstrap";
 import { Button } from "react-bootstrap";
+import { useState } from 'react';
+
 
 function App() {
+  var [s, start] = useState("5");
+
   return (
     <div className="App">
       <Header></Header>
@@ -54,24 +58,50 @@ function App() {
 
               <h3 className="line-1 anim-typewriter">Sports</h3>
 
-
             </Carousel.Caption>
           </Carousel.Item>
         </Carousel>
         <h1 className="ed">Principal's Desk</h1>
         <div className="am">
-        <Card style={{ width: '32rem', height: '25rem' }}>
-          <Card.Img variant="top" src={fourth} style={{ width: '32rem', height: '15rem' }} />
+          <Card style={{ width: '32rem', height: '25rem' }}>
+            <Card.Img variant="top" src={fourth} style={{ width: '32rem', height: '15rem' }} />
+            <Card.Body>
+              <Card.Title className="gh">Principal ~ Mrs. R.M Shukla(M.A,B.Ed)</Card.Title>
+              <Card.Text className="hj">
+                A good student is who shares his knowledge with others too.
+              </Card.Text>
+              <Button variant="primary">Know More</Button>
+            </Card.Body>
+          </Card>
+        </div>
+
+      </div>
+      <div className="df">
+        <Card className="kl"
+          bg={'Success'.toLowerCase()}
+
+          text={'Success'.toLowerCase() === 'light' ? 'dark' : 'white'}
+          style={{ width: '40vw',height:'70vh' }}
+          className="mb-2"
+        >
+          <Card.Header>Notice Board</Card.Header>
           <Card.Body>
-            <Card.Title className="gh">Principal ~ Mrs. R.M Shukla(M.A,B.Ed)</Card.Title>
-            <Card.Text className="hj">
-              A good student is who shares his knowledge with others too.
-            </Card.Text>
-            <Button variant="primary">Know More</Button>
+            <marquee width="100%" direction="up" onMouseOver="this.stop()" onMouseOut="this.start()" height="67%" bgcolor="white">
+              <ul className="uu">
+                <li className="ss">This is a sample scrolling text that has scrolls in the upper direction.</li>
+                <li className="ss">This is a sample scrolling text that has scrolls in the upper direction.</li>
+                <li className="ss">This is a sample scrolling text that has scrolls in the upper direction.</li>
+                <li className="ss">This is a sample scrolling text that has scrolls in the upper direction.</li>
+                <li className="ss">This is a sample scrolling text that has scrolls in the upper direction.</li>
+                <li className="ss">This is a sample scrolling text that has scrolls in the upper direction.</li>
+                <li className="ss">This is a sample scrolling text that has scrolls in the upper direction.</li>
+                <li className="ss">This is a sample scrolling text that has scrolls in the upper direction.</li>
+              </ul>
+            </marquee>
           </Card.Body>
         </Card>
-        </div>
       </div>
+
     </div>
   );
 }
