3a033e06fe32c019b6fa833297da1e6d306f6234
Md Shahbaz Alam <shahbazalam75508@gmail.com>
2021-08-23 02:13:33 +0530

    Commit

--- src/MyComponents/footer/footer.js
+++ src/MyComponents/footer/footer.js
@@ -66,7 +66,7 @@ const Footer = () => {
                 </span>
               </i>
             </FooterLink>
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d115408.23982812326!2d82.92106856136766!3d25.320746099824227!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x398e2db76febcf4d%3A0x68131710853ff0b5!2sVaranasi%2C%20Uttar%20Pradesh!5e0!3m2!1sen!2sin!4v1629664419593!5m2!1sen!2sin" style={width="400",height="300" , allowfullscreen="", loading="lazy"}></iframe>
+     
           </Column>
         </Row>
       </Container>
